ffbd9456a563048e7e31daf4c35b25eb3bb921f3
rizkirmdhn1215 <rizkirmdhn1215@gmail.com>
2024-12-12 22:16:09 +0700

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">404</h1>
+        <p className="text-gray-600">Halaman tidak ditemukan</p>
+      </div>
+    </div>
+  );
+} 
